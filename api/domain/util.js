953f157776472c64d4008b261788a1a4a20b6a50
Marque Davis <mdavis777@gmail.com>
2017-10-01 14:20:36 -0400

    Stop bloating logs

--- api/domain/util.js
+++ api/domain/util.js
@@ -3,13 +3,10 @@
  * @param taskFactories Array of nilary functions to lazily create a promise
  */
 exports.serializeTasks = (taskFactories) => {
-  let counter = 0;
   return taskFactories.reduce((promiseChain, taskFactory) => {
     return promiseChain.then(
       async chainResults => {
-        console.log(`Starting ${++counter}`);
         const currentResult = await taskFactory();
-        console.log(`Finished ${counter}`);
         return [...chainResults, currentResult];
       }
     );
