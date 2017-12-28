e6f088ad6590a20b98b57df74be5f9ba87c2c465
Marque Davis <mdavis777@gmail.com>
2017-12-28 12:04:39 -0500

    Merge Inserts and Updates into one tab since TH handles upserts

--- api/domain/territoryHelper/export/createExcelFile.js
+++ api/domain/territoryHelper/export/createExcelFile.js
@@ -1,15 +1,16 @@
 const XLSX = require('xlsx');
+const compact = require('lodash/compact');
+const flatten = require('lodash/flatten');
 
 exports.requires = ['externalLocations'];
 exports.returns = 'buffer';
 exports.handler = async function createExcelFile({ inserts, updates, deletes }) {
   const workbook = {
     Sheets: {
-      'New': XLSX.utils.json_to_sheet(inserts || []),
-      'Updates': XLSX.utils.json_to_sheet(updates || []),
-      'Deletions': XLSX.utils.json_to_sheet(deletes || []),
+      'Updated': XLSX.utils.json_to_sheet(compact(flatten([inserts, updates]))),
+      'Deleted': XLSX.utils.json_to_sheet(compact(deletes)),
     },
-    SheetNames: ['New', 'Updates', 'Deletions'],
+    SheetNames: ['Updated', 'Deleted'],
   };
 
   return await XLSX.write(workbook, { bookType: 'xlsx', type: 'buffer', compression: true });
