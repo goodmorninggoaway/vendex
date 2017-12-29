4379e5e8217e09676eefbaf5d8b61f31ace289b4
Marque Davis <mdavis777@gmail.com>
2017-12-29 09:53:58 -0500

    Using hapi-moon boilerplate, get a running server that doesn't work

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const Glue = require('glue');
+const serverConfig = require('./config/manifest');
+
+// this is the line we mention in manifest.js
+// relativeTo parameter should be defined here
+const options = {...serverConfig.options, relativeTo: __dirname};
+
+const startServer = async function () {
+  try {
+    const server = await Glue.compose(serverConfig.manifest, options);
+    await server.start();
+    console.log('server started');
+  }
+  catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+startServer();
