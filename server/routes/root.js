ff65ec5bb8d0a7b3aebaa46fa96bceafde3f36b5
Marque Davis <mdavis777@gmail.com>
2018-02-25 14:43:23 -0500

    Update default redirect

--- server/routes/root.js
+++ server/routes/root.js
@@ -6,7 +6,7 @@ exports.plugin = {
         path: '/',
         options: {
           async handler(req, h) {
-            return h.redirect('/ui/users');
+            return h.redirect('/ui');
           },
         },
       },
