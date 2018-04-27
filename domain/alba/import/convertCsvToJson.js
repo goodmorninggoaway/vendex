d44c81ff1176b16bff77672d53637aba9b55f2d9
Marque Davis <mdavis777@gmail.com>
2018-04-26 21:48:33 -0400

    Update to make Alba import work with synthetic "Alba" exports.

--- domain/alba/import/convertCsvToJson.js
+++ domain/alba/import/convertCsvToJson.js
@@ -6,7 +6,7 @@ exports.handler = async function convertCsvToJson({ tsv }) {
   return new Promise((resolve, reject) => {
     const rows = [];
 
-    csv({ delimiter: '\t' })
+    csv({ delimiter: ['\t', ','] })
       .fromString(tsv)
       .on('json', jsonObj => {
         rows.push(jsonObj);
@@ -14,10 +14,6 @@ exports.handler = async function convertCsvToJson({ tsv }) {
       .on('done', error => {
         if (error) {
           return reject(error);
-        }
-
-        if (rows[0] && !rows[0].Address_ID) {
-          return reject('Missing header row');
         }
         resolve(rows);
       });
