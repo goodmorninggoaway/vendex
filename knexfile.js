4685c3c3a2d25bbb903bbd630979e918f18ac05b
Marque Davis <mdavis777@gmail.com>
2022-02-21 13:31:06 -0500

    Handle updated/outdated Heroku pg changes

--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,21 @@
-require('dotenv').config();
+require("dotenv").config();
 let connection;
 if (process.env.DATABASE_URL) {
   connection = {
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.DATABASE_SSL !== 'false' && !!process.env.DATABASE_SSL,
+    ssl: process.env.DATABASE_SSL !== "false" && !!process.env.DATABASE_SSL ? {
+      rejectUnauthorized: false
+    } : false
   };
 }
 
 module.exports = {
-  client: 'pg',
+  client: "pg",
   connection,
   migrations: {
-    directory: './domain/dataAccess/migrations',
+    directory: "./domain/dataAccess/migrations"
   },
   seeds: {
-    directory: './domain/dataAccess/seeds',
-  },
+    directory: "./domain/dataAccess/seeds"
+  }
 };
